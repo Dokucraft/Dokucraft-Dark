e7b300feb97c2468039dfee1aefa3d6ba6d403bd
Even Torset <eventorset@gmail.com>
2022-05-27 20:14:27 +0200

    Added the shader changes from Light.

--- assets/minecraft/shaders/flavor.glsl
+++ assets/minecraft/shaders/flavor.glsl
@@ -8,3 +8,15 @@
 // Loading screen background color
 #define LOADING_BG_DARK_COLOR vec3(0.101, 0.141, 0.086)
 #define LOADING_BG_COLOR vec3(0.101, 0.141, 0.086)
+
+// End sky colors
+#define END_SKY_RIFT_COLOR vec3(0.7, 0.3, 0.15)
+#define END_SKY_RIFT_EDGE_COLOR vec3(32.0, 40.0, 70.0)
+
+#define END_SKY_STARS_OUTSIDE_BASE_COLOR vec3(0.9, 0.8, 0.6)
+#define END_SKY_STARS_OUTSIDE_COLOR_VARIANCE vec3(0.1, 0.1, 0.2)
+#define END_SKY_NEBULAE_OUTSIDE_COLOR vec3(0.8, 0.3, 0.8)
+
+#define END_SKY_STARS_INSIDE_BASE_COLOR vec3(1.0, 1.2, 2.0)
+#define END_SKY_STARS_INSIDE_COLOR_VARIANCE vec3(0.1, 0.2, 0.1)
+#define END_SKY_NEBULAE_INSIDE_COLOR vec3(0.4, 0.5, 0.8)
